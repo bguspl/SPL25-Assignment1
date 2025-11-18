ebdc3b037f4d88afc6204ed604cb696b5cfdc7b5
Yuval Birger <birgery@post.bgu.ac.il>
2025-11-18 17:43:11 +0200

    Cache and DJControllerService

--- src/DJControllerService.cpp
+++ src/DJControllerService.cpp
@@ -10,8 +10,26 @@ DJControllerService::DJControllerService(size_t cache_size)
  * TODO: Implement loadTrackToCache method
  */
 int DJControllerService::loadTrackToCache(AudioTrack& track) {
-    // Your implementation here 
-    return 0; // Placeholder
+    // Your implementation here
+    if (cache.contains(track.get_title())) {    //HIT
+        cache.get(track.get_title());
+        return 1;
+    }
+    else {                          //MISS
+        PointerWrapper<AudioTrack> pw = track.clone();
+        AudioTrack* track_ptr = pw.release();
+        if (!track_ptr) {
+            std::cout << "[ERROR] Track: \""
+                << track.get_title() << "\" failed to clone " << std::endl;
+            return 0;
+        }
+        track_ptr->load();
+        track_ptr->analyze_beatgrid();
+        if (cache.put(PointerWrapper<AudioTrack>(track_ptr))) {
+            return -1;               //with eviction
+        }
+        return 0;                    //no eviction
+    }
 }
 
 void DJControllerService::set_cache_size(size_t new_size) {
@@ -29,5 +47,5 @@ void DJControllerService::displayCacheStatus() const {
  */
 AudioTrack* DJControllerService::getTrackFromCache(const std::string& track_title) {
     // Your implementation here
-    return nullptr; // Placeholder
+    return cache.get(track_title);
 }
