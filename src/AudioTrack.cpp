9c096246655144dfedc9d3ac214bb7a3a7bb0aaa
Ahmad Rabia <ahmaddrabia@gmail.com>
2025-11-20 14:28:48 +0000

    mnwb

--- src/AudioTrack.cpp
+++ src/AudioTrack.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <cstring>
 #include <random>
-// hi
-//no
+// ya mnwb
 AudioTrack::AudioTrack(const std::string& title, const std::vector<std::string>& artists, 
                       int duration, int bpm, size_t waveform_samples)
     : title(title), artists(artists), duration_seconds(duration), bpm(bpm), 
